e27dcd7d4681e2fa40d964f12abf75e9293b073e
Justin Dubs <jtdubs@gmail.com>
2024-04-11 22:33:22 -0400

    pull in home-manager

--- flake.nix
+++ flake.nix
@@ -4,15 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
   {
     nixosConfigurations = {
       decl = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/decl/configuration.nix
+          # home-manager.nixosModules.home-manager {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.users.jtdubs = import ./users/jtdubs/home.nix;
+          # }
         ];
         specialArgs = {
           inherit inputs;
