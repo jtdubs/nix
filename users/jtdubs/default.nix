877bce2d21b4ed77a9302c2d9c00d18d54ed38b3
Justin Dubs <jtdubs@gmail.com>
2024-04-15 12:22:31 -0400

    wob config

--- users/jtdubs/default.nix
+++ users/jtdubs/default.nix
@@ -64,6 +64,7 @@
     "tmux" = { source = xdg/tmux; recursive = true; };
     "tofi" = { source = xdg/tofi; recursive = true; };
     "waybar" = { source = xdg/waybar; recursive = true; };
+    "wob" = { source = xdg/wob; recursive = true; };
     # Files
     "starship.toml".source = xdg/starship.toml;
     "nvim/colors/custom-theme.vim".source = xdg/nvim/colors/custom-theme.vim;
@@ -94,8 +95,11 @@
       t = "tmux new-session -A -s";
     };
     initExtra = ''
-      bindkey "''${key[Up]}" history-beginning-search-backward
-      bindkey "''${key[Down]}" history-beginning-search-forward
+      autoload -U history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "''${key[Up]}" history-beginning-search-backward-end
+      bindkey "''${key[Down]}" history-beginning-search-forward-end
     '';
   };
   programs.starship.enable = true;
