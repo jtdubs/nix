2d2185413437ec70bf60cdd429954f7ff4395ce4
Justin Dubs <jtdubs@gmail.com>
2024-04-19 15:26:38 -0400

    nord all the things

--- hosts/decl/default.nix
+++ hosts/decl/default.nix
@@ -55,18 +55,18 @@
   programs.zsh.enable = true;
   programs.sway.enable = true;
   environment.systemPackages = with stable; [
-    brlaser         # printer drivers
+    brlaser # printer drivers
     curl
-    foot            # sway's default shell
-    home-manager    # home-manager
+    foot # sway's default shell
+    home-manager # home-manager
     git
     neovim
-    sway            # tiling window manager
+    sway # tiling window manager
     swayidle
     swaylock
-    tmux            # terminal multiplexer
+    tmux # terminal multiplexer
     xdg-utils
-    wl-clipboard    # wayland clipboard utils
+    wl-clipboard # wayland clipboard utils
   ];
 
   # Environment variables
--- users/jtdubs/default.nix
+++ users/jtdubs/default.nix
@@ -12,30 +12,30 @@
     gimp
     google-chrome
     # cli tools
-    fzf             # fuzzy search
+    fzf # fuzzy search
     # utils
-    feh             # image viewer
-    gnome.seahorse  # keyring
-    libnotify       # desktop notifications
-    nerdfonts       # fonts
-    nixpkgs-fmt     # nix formatter
-    pass            # password store
-    units           # dimensional analysis
+    feh # image viewer
+    gnome.seahorse # keyring
+    libnotify # desktop notifications
+    nerdfonts # fonts
+    nixpkgs-fmt # nix formatter
+    pass # password store
+    units # dimensional analysis
     # sway utils
-    brightnessctl   # backlight control
-    grim            # screenshot tool
-    pavucontrol     # pulseaudio sound ui
-    pulseaudio      # pulseaudio
-    mako            # notification messages
-    slurp           # screenshot tool
-    tofi            # launch menu
-    waybar          # status bar
-    wlay            # output manager
-    wob             # notification bars
+    brightnessctl # backlight control
+    grim # screenshot tool
+    pavucontrol # pulseaudio sound ui
+    pulseaudio # pulseaudio
+    mako # notification messages
+    slurp # screenshot tool
+    tofi # launch menu
+    waybar # status bar
+    wlay # output manager
+    wob # notification bars
   ]) ++ (with unstable; [
-    alacritty       # terminal emulator
-    blender         # 3d modeling
-    btop            # better top
+    alacritty # terminal emulator
+    blender # 3d modeling
+    btop # better top
   ]);
 
   # Large pointer
@@ -62,13 +62,11 @@
     "mako" = { source = xdg/mako; recursive = true; };
     "sway" = { source = xdg/sway; recursive = true; };
     "swaylock" = { source = xdg/swaylock; recursive = true; };
-    "tmux" = { source = xdg/tmux; recursive = true; };
     "tofi" = { source = xdg/tofi; recursive = true; };
     "waybar" = { source = xdg/waybar; recursive = true; };
     "wob" = { source = xdg/wob; recursive = true; };
     # Files
     "starship.toml".source = xdg/starship.toml;
-    "nvim/colors/custom-theme.vim".source = xdg/nvim/colors/custom-theme.vim;
   };
 
   # Environment variables
@@ -119,12 +117,30 @@
       vim-highlightedyank
       vim-matchup
       vim-yaml
+      nord-vim
     ];
   };
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+  };
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    baseIndex = 1;
+    newSession = true;
+    escapeTime = 0;
+    plugins = with stable.tmuxPlugins; [
+      better-mouse-mode
+      prefix-highlight
+      nord
+    ];
+    terminal = "tmux-256color";
+    keyMode = "vi";
+    customPaneNavigationAndResize = true;
+    historyLimit = 1000;
+    extraConfig = lib.fileContents xdg/tmux/tmux.conf;
   };
 
   # Generate an HTML manual
