433c005e52def4652ea6a7f574654afb0028e02f
Justin Dubs <jtdubs@gmail.com>
2024-04-19 16:37:24 -0400

    fix gdm pam config

--- hosts/decl/default.nix
+++ hosts/decl/default.nix
@@ -1,4 +1,4 @@
-{ disko, nixos-hardware, stable, unstable, ... }:
+{ lib, disko, nixos-hardware, stable, unstable, ... }:
 {
   imports =
     [
@@ -96,7 +96,24 @@
 
   # Authentication
   services.fprintd.enable = true;
-  security.pam.services.login.fprintAuth = true;
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint.text = ''
+    auth       required                    pam_shells.so
+    auth       requisite                   pam_nologin.so
+    auth       requisite                   pam_faillock.so      preauth
+    auth       required                    ${stable.fprintd}/lib/security/pam_fprintd.so
+    auth       optional                    pam_permit.so
+    auth       required                    pam_env.so
+    auth       [success=ok default=1]      ${stable.gnome.gdm}/lib/security/pam_gdm.so
+    auth       optional                    ${stable.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+    account    include                     login
+
+    password   required                    pam_deny.so
+
+    session    include                     login
+    session    optional                    ${stable.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+  '';
   security.sudo.wheelNeedsPassword = false;
 
   # Sound
