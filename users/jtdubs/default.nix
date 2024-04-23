88b84c2b5c7d932fd987abb702e423bce0ca1996
Justin Dubs <jtdubs@gmail.com>
2024-04-23 12:28:35 -0400

    pull out theme.nix

--- users/jtdubs/default.nix
+++ users/jtdubs/default.nix
@@ -1,36 +1,10 @@
 { lib, stable, unstable, vscode-extensions, nix-colors, ... }:
 {
-  # Nord-inspired theme
-  colorScheme = {
-    slug = "nix";
-    name = "Nix";
-    author = "Justin Dubs <jtdubs@gmail.com>";
-    palette = {
-      # Polar Night
-      night0 = "2E3440";
-      night1 = "3B4252";
-      night2 = "434C5E";
-      night3 = "4C566A";
-      # Snow Storm
-      snow0 = "D8DEE9";
-      snow1 = "E5E9F0";
-      snow2 = "ECEFF4";
-      # Frost
-      frost0 = "8FBCBB";
-      frost1 = "88C0D0";
-      frost2 = "81A1C1";
-      frost3 = "5E81AC";
-      # Aurora
-      auroraRed = "BF616A";
-      auroraOrange = "D08770";
-      auroraYellow = "EBCB8B";
-      auroraGreen = "A3BE8C";
-      auroraPurple = "B48EAD";
-    };
-  };
-
   imports = [
+    # Theming
     nix-colors.homeManagerModules.default
+    ./theme.nix
+    # Everything else
     ./cursor.nix
     ./environment.nix
     ./packages
