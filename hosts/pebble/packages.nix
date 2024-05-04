d47ddbd3b028b2794190b064696bd5dce4409a41
Justin Dubs <jtdubs@gmail.com>
2024-05-03 22:37:00 -0400

    add file and nix-ld

--- hosts/pebble/packages.nix
+++ hosts/pebble/packages.nix
@@ -2,11 +2,13 @@
 {
   programs.zsh.enable = true;
   programs.sway.enable = true;
+  programs.nix-ld.enable = true;
   environment.systemPackages = with stable; [
     brlaser # printer drivers
     curl
     foot # sway's default shell
     home-manager # home-manager
+    file
     git
     sway # tiling window manager
     swayidle
